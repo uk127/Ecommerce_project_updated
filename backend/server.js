97e74525999c79b20104188e821b2346fd941898
uk127 <unnik9218@gmail.com>
2026-06-08 22:09:09 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.use(cookieParser());
 // Enable CORS for all routes
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", process.env.FRONTEND_URL],
     credentials: true,
   })
 );
